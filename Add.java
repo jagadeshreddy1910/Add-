88cde81df975975c4b401409c338dad1891efaa3
jagadeshreddy1910 <jagadeshreddy779@gmail.com>
2025-01-29 00:53:34 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,15 @@
+
+    public class Add {
+        public static void main(String[] args) {
+            int a = 3;
+            int b= 8;
+            int add = a+b;
+            int sub = a-b;
+            float div = a/b;
+            System.out.println(add);
+            System.out.println(sub);
+            System.out.println(div);
+    
+        }
+    }   
+
